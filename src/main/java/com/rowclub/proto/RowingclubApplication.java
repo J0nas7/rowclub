2b2ce9506878121d0b4e7bf638bbe3cb724d2260
Jonas Sørensen <jonas7793@gmail.com>
2018-05-01 13:59:58 +0200

    First commit

---
+++ src/main/java/com/rowclub/proto/RowingclubApplication.java
@@ -0,0 +1,12 @@
+package com.rowclub.proto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RowingclubApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RowingclubApplication.class, args);
+    }
+}
