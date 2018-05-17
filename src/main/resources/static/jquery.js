988e2d2a27e9adc13a5d8d3eeb014b78c0523378
Jonas Sørensen <jonas7793@gmail.com>
2018-05-17 12:31:44 +0200

    JavaScript + jQuery

--- src/main/resources/static/jquery.js
+++ src/main/resources/static/jquery.js
@@ -1,5 +1,7 @@
 $(document).ready(function() {
     if ($(".moreGuests").length) {
-        
+        $(".moreGuests .cta").click(function() {
+            $(".moreGuests input").clone().appendTo(".theGuests");
+        });
     }
 });
