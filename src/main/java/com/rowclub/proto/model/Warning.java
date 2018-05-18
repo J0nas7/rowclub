4f32a927b851a8b617de81ee89fb9cd3dd5fb46e
DESKTOP-3IJJ0DR\Jacob <JacobViehweg@hotmail.com>
2018-05-18 09:46:48 +0200

    Færdiggørelse af warning klasse

--- src/main/java/com/rowclub/proto/model/Warning.java
+++ src/main/java/com/rowclub/proto/model/Warning.java
@@ -1,9 +1,60 @@
 package com.rowclub.proto.model;
 
+import java.util.Date;
+
 public class Warning {
 
-    
+    private int warningId;
+    private String info;
+    private int boatTripId;
+    private Date DateStamp;
+    private int TimeStamp;
 
+    public Warning(int warningId, String info, int boatTripId, Date dateStamp, int timeStamp) {
+        this.warningId = warningId;
+        this.info = info;
+        this.boatTripId = boatTripId;
+        DateStamp = dateStamp;
+        TimeStamp = timeStamp;
+    }
 
+    public int getWarningId() {
+        return warningId;
+    }
 
+    public void setWarningId(int warningId) {
+        this.warningId = warningId;
+    }
+
+    public String getInfo() {
+        return info;
+    }
+
+    public void setInfo(String info) {
+        this.info = info;
+    }
+
+    public int getBoatTripId() {
+        return boatTripId;
+    }
+
+    public void setBoatTripId(int boatTripId) {
+        this.boatTripId = boatTripId;
+    }
+
+    public Date getDateStamp() {
+        return DateStamp;
+    }
+
+    public void setDateStamp(Date dateStamp) {
+        DateStamp = dateStamp;
+    }
+
+    public int getTimeStamp() {
+        return TimeStamp;
+    }
+
+    public void setTimeStamp(int timeStamp) {
+        TimeStamp = timeStamp;
+    }
 }
