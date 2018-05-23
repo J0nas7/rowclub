84e132a966d2d5c3c60b3a792569023fbb1906ed
Unknown <caspermklein@gmail.com>
2018-05-23 15:41:10 +0200

    Auto stash before merge of "master" and "origin/master"
    Main med testkode til opvisning.

--- src/main/java/com/rowclub/proto/RowingclubApplication.java
+++ src/main/java/com/rowclub/proto/RowingclubApplication.java
@@ -1,8 +1,6 @@
 package com.rowclub.proto;
 
-import com.rowclub.proto.model.Boat;
-import com.rowclub.proto.model.Member;
-import com.rowclub.proto.repository.BoatTripsDbRepository;
+import com.rowclub.proto.repository.*;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -15,10 +13,32 @@ public class RowingclubApplication {
     }
 
     public static void main(String[] args) throws SQLException {
-        SpringApplication.run(RowingclubApplication.class, args);
-        
 
-        System.out.println();
+        //SpringApplication.run(RowingclubApplication.class, args);
+
+        //Test kode til fremvisning hvor UI ikke kan vise funtionalitet (Alt efter denne kommentar skal slettes når UI er tilstrækkelig)
+        MemberDbRepository memberDbRepository = new MemberDbRepository();
+        BoatDbRepository boatDbRepository = new BoatDbRepository();
+        BoatTripLinkDbRepository boatTripLinkDbRepository = new BoatTripLinkDbRepository();
+        BoatTripsDbRepository boatTripsDbRepository = new BoatTripsDbRepository();
+        PreDetTripsDbRepository preDetTripsDbRepository = new PreDetTripsDbRepository();
+        SeasonDbRepository seasonDbRepository = new SeasonDbRepository();
+        WarningDbRepository warningDbRepository = new WarningDbRepository();
+
+        //Member (Create, Update, Delete)
+        //memberDbRepository.createMember("Lars","Andersen","02/10/1985","05/11/1999","12345678",Boolean.FALSE,Boolean.FALSE,"Junior","No Image");
+        //memberDbRepository.updateMember(1,"","","","","87654321",null,null,"","");
+        //memberDbRepository.deleteMember(1);
+
+        //Boat (Create, Update, Delete)
+        //boatDbRepository.createBoat("Dan","Out-rigger","idle",8);
+        //boatDbRepository.updateBoat(1,"","","På vandet",0);
+        //boatDbRepository.deleteBoat(1);
+
+        //BoatTrip (Create, Update, Delete)
+
+
+
     }
 
 }
