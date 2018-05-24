3e03c9a86e193876a9335c399c8c8b83be65de05
Jonas Sørensen <jonas7793@gmail.com>
2018-05-24 13:11:46 +0200

    DDL

--- src/main/java/com/rowclub/proto/controller/DatabaseController.java
+++ src/main/java/com/rowclub/proto/controller/DatabaseController.java
@@ -26,6 +26,85 @@ public class DatabaseController {
         } catch (Exception e) {
             e.printStackTrace();
         }
+        // CREATE DATABASE krdatabase /*!40100 DEFAULT CHARACTER SET utf8 */;
+        /*
+        USE krdatabase;
+
+        CREATE TABLE protokol_boat (
+                BoatID int(11) NOT NULL AUTO_INCREMENT,
+                Name varchar(45) NOT NULL,
+        Type varchar(45) NOT NULL,
+        status varchar(45) NOT NULL DEFAULT 'Idle',
+                seats int(11) NOT NULL,
+        PRIMARY KEY (BoatID)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+        CREATE TABLE protokol_boattrip (
+                BoatTripID int(11) NOT NULL AUTO_INCREMENT,
+                fkBoatID int(11) NOT NULL,
+        Distance double NOT NULL DEFAULT '0',
+                EstDuration int(11) DEFAULT NULL,
+        DateStamp date NOT NULL,
+        fkSeasonID int(11) DEFAULT NULL,
+        CompletionTime int(11) DEFAULT NULL,
+        TimeStamp int(11) NOT NULL,
+        PRIMARY KEY (BoatTripID),
+                KEY BoatID_idx (fkBoatID),
+                KEY SeasonID_idx (fkSeasonID)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+        CREATE TABLE protokol_member (
+                MemberID int(11) NOT NULL AUTO_INCREMENT,
+                FirstName varchar(45) DEFAULT NULL,
+        LastName varchar(45) DEFAULT NULL,
+        DoB date DEFAULT NULL,
+        RegDate date DEFAULT NULL,
+        Phone varchar(45) DEFAULT NULL,
+        Admin tinyint(4) NOT NULL DEFAULT '0',
+                Matey tinyint(4) NOT NULL DEFAULT '0',
+                Type varchar(45) DEFAULT 'guest',
+                PhotoRef varchar(45) DEFAULT NULL,
+        PRIMARY KEY (MemberID)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+        CREATE TABLE protokol_predeterminedtrips (
+                PreID int(11) NOT NULL AUTO_INCREMENT,
+                Location varchar(45) NOT NULL,
+        Distance double NOT NULL,
+                PreEstDuration int(11) NOT NULL,
+        PRIMARY KEY (PreID)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+        CREATE TABLE protokol_season (
+                SeasonID int(11) NOT NULL AUTO_INCREMENT,
+                Year int(11) NOT NULL,
+        StartDate date DEFAULT NULL,
+        EndDate date DEFAULT NULL,
+        PRIMARY KEY (SeasonID)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+        CREATE TABLE protokol_warning (
+                WarningID int(11) NOT NULL AUTO_INCREMENT,
+                Info varchar(45) NOT NULL,
+        fkBoatTripID int(11) NOT NULL,
+        PRIMARY KEY (WarningID),
+                KEY BoatTripID_idx (fkBoatTripID),
+                KEY BoatTripID2_idx (fkBoatTripID),
+                CONSTRAINT BoatTripID2 FOREIGN KEY (fkBoatTripID) REFERENCES protokol_boattrip (BoatTripID) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+        CREATE TABLE protokol_boattriplink (
+                BoatTripLinkID int(11) NOT NULL AUTO_INCREMENT,
+                fkMemberID int(11) NOT NULL,
+        fkBoatTripID int(11) NOT NULL,
+        PRIMARY KEY (BoatTripLinkID),
+                KEY MemberID_idx (fkMemberID),
+                KEY BoatTripID_idx (fkBoatTripID),
+                KEY BoatTripID_idx2 (fkBoatTripID),
+                CONSTRAINT BoatTripID FOREIGN KEY (fkBoatTripID) REFERENCES protokol_boattrip (BoatTripID) ON DELETE NO ACTION ON UPDATE NO ACTION,
+                CONSTRAINT MemberID FOREIGN KEY (fkMemberID) REFERENCES protokol_member (MemberID) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+         */
     }
     // Database configuration end
 
